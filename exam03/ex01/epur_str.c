6c1e17e262d52b9af54f86c8d4c090d445475a27
1McLongLong <85419730+1McLongLong@users.noreply.github.com>
2023-08-02 23:49:57 +0100

    Update epur_str.c

--- exam03/ex01/epur_str.c
+++ exam03/ex01/epur_str.c
@@ -42,3 +42,31 @@ int	 main(int argc, char **argv)
 	write(1, "\n", 1);
 	return 0;
 }
+
+// Second Way
+
+void	epur_str(char *str)
+{
+	if (*str)
+	{
+		while (*str && (*str == ' ' || *str == '\t'))
+			str++;
+		while (*str && *str != ' ' && *str != '\t')
+		{
+			write(1, &*str, 1);
+			str++;
+		}
+		while (*str)
+		{
+			while (*str && (*str == ' ' || *str == '\t'))
+				str++;
+			if (*str)
+				write(1, " ", 1);
+			while (*str && *str != ' ' && *str != '\t')
+			{
+				write(1, &*str, 1);
+				str++;
+			}
+		}
+	}
+}
