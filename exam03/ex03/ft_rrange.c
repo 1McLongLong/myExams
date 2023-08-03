678037c08f67916d0beb206dae8aa84c4895699d
1McLongLong <85419730+1McLongLong@users.noreply.github.com>
2023-08-03 16:39:59 +0100

    Update ft_rrange.c

--- exam03/ex03/ft_rrange.c
+++ exam03/ex03/ft_rrange.c
@@ -9,97 +9,49 @@ return a pointer to the first value of the array.
 #include <stdlib.h>
 #include <unistd.h>
 
-
-int ft_abs(int x)
-{
-	return x < 0 ? -x : x;
-}   
-
-void ft_putchar(char c)
-{
-    write(1, &c, 1);
-}
-
-int ft_atoi(char *str)
-{
-    int sign, result;
-    while(*str == ' ' || *str == '\n' || *str == '\t' || *str == '\r' || *str == '\v' || *str == '\f')
-        str++;
-
-    sign = 1;
-    if (*str == '+' || *str == '-')
-    {
-        if(*str == '-')
-        {
-            sign = -sign;   
-        }
-        str++;
-    }
-    result = 0;
-    while(*str >= '0' && *str <= '9')
-    {
-        result = result * 10 + (*str - '0');
-        str++;
-    }
-    return result * sign;   
-}
-
-void ft_putnbr(int nb)
-{
-    if (nb < 0)
-    {
-        ft_putchar('-');
-        nb = -nb;
-    }
-    if (nb < 10)
-    {
-        ft_putchar(nb + '0');
-    }
-    else
-    {
-        ft_putnbr(nb / 10);
-        ft_putnbr(nb % 10);
-    }
-}
-
-
 int *ft_rrange(int start, int end)
 {
     int *r;
     int len;
-    len = (end >= start) ? end - start + 1 : start - end + 1;
 
-    if(!(r = (int*)malloc(len * sizeof(int))))
+    if (end >= start) 
+        len = end - start + 1;
+    else 
+        len = start - end + 1;
+    
+
+    r = (int*)malloc(len * sizeof(int));
+    if (r == 0) 
         return 0;
-    while(len--)
-        r[len] = end >= start ? start++ : start--;
+
+    while (len--) 
+    {
+        if (end >= start) 
+            r[len] = start++;
+        else 
+            r[len] = start--;
+    }
     return r;           
 }
-
 
 int main(int argc, char **argv)
 {
     if (argc != 3)
         return 0;
 
-    int start = ft_atoi(argv[1]);
-    int end = ft_atoi(argv[2]);
+    int start = atoi(argv[1]);
+    int end = atoi(argv[2]);
 
     int *range = ft_rrange(start, end);
 
     int i = 0;
-    while (i < ft_abs(start - end) + 1)
+    while (i < abs(start - end) + 1)
     {
-        ft_putnbr(range[i]);
-        ft_putchar(' ');
+        printf("%d ", range[i]);
         i++;
     }
-    
-    ft_putchar('\n');
+    printf("\n");
     free(range);
 
     return 0;
 }
-
-
-
